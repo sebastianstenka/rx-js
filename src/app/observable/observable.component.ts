634d8fdb96965241bb4c993683075ac5b1a1f651
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-11-12 14:59:48 +0100

    Create first component

---
+++ src/app/observable/observable.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-observable',
+  templateUrl: './observable.component.html',
+  styleUrls: ['./observable.component.scss'],
+})
+export class ObservableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
